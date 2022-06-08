8ae8cb786fe6721be5f1bd69e8a87aa1bb276197
Alexander Grasser <a01526871@unet.univie.ac.at>
2022-06-08 11:41:53 +0200

    Added trailing slash handling

--- makereplicates.sh
+++ makereplicates.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-path1=$1
 
-desiredcopies=${2:-3} # number of desired copies. Defaults to three replicates = two copies
-current_copy=1
 
 
 Help()
@@ -22,7 +19,7 @@ Help()
     echo "So if you specify you want 6 replicates, it wll make 5 copies: -2, -3, -4, -5, -6"
     echo
     echo "------------FILE HANDLING---------------------------"
-    echo "Files must be specified as relative path"
+    echo "Source folder must be specified as relative path, ending with -1/"
     echo "Examples of correct commands:"
     echo "tf_makereplicates ./replicate-1/ #Makes 2 replicates (replicate-2 and replicate-3) in the local directory"
     echo "tf_makereplicates ~/tf_production/struc22/23to28/5ns_k3ex3-scaling-1/ 10 # Makes 10 replicates (-2 to -10) in the same location as the source"
@@ -37,12 +34,24 @@ while getopts ":h" option; do
     esac
 done
 
+path1=$1
+
+desiredcopies=${2:-3} # number of desired copies. Defaults to three replicates = two copies
+current_copy=1
+
+if [[ ${path1: -1} != "/" ]]
+then
+    echo "No trailing slash - adding"
+    
+    path1="${path1}/"
+
+fi
 
 while [ $current_copy -ne $desiredcopies ]
 do
     current_copy=$(( $current_copy +1 ))
     targetdir=${path1::-2}$current_copy
-    echo "copying from ${path1} to ${targetdir}"
+    echo "copying from ${path1} to ${targetdir}/"
     cp -rf ${path1} ${targetdir}
 
     
