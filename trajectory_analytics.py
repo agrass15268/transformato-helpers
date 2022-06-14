418620d63422e064d56b4869d42e6bb1e7779c95
Alexander Grasser <a01526871@unet.univie.ac.at>
2022-06-14 09:10:17 +0200

    Improvements to trajectory-analytics, including relative distance

--- trajectory_analytics.py
+++ trajectory_analytics.py
@@ -8,16 +8,20 @@ import yaml
 import transformato.restraints as tfrs
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 logging.basicConfig(level=logging.DEBUG,stream=sys.stdout)
 logger=logging.getLogger(__name__)
 
 
 
-parser=argparse.ArgumentParser(description="Select which run you want to analyse. By defualt, visualizes distances as experienced by the restraints.")
+parser=argparse.ArgumentParser(description="Select which run you want to analyse. By default, visualizes distances as experienced by the restraints.")
 parser.add_argument("rundir",nargs="+",help="The run (replicate) directories containing the trajectories you want to analyze")
 parser.add_argument("--altrestraints",help="For systems without restraints, you may specify an alternative restraints.yaml")
-
+parser.add_argument("--csv",help="Write out a csv named <rundir>.csv")
+parser.add_argument("-s","--step",help="How many frames to skip between analytics runs",default=100,type=int)
+parser.add_argument("-nv","--novis",help="Suppress plot output",type=bool)
+parser.add_argument("-rd","--relative-distance",help="Display relative distance (r-r0) instead of absolute distance.")
 
 args=parser.parse_args()
 
@@ -68,7 +72,7 @@ class ReplicateToAnalyze():
             self.logger.info(f"Loaded restraints configuration at {self.restraint_path}")
 
         self.logger.info(f"Restraints for this run were set as: {self.config['simulation']['restraints']}")
-        self.restraints_to_analyze=tfrs.CreateRestraintsFromConfig(self.config,self.pdbpath)
+        self.restraints_to_analyze=tfrs.create_restraints_from_config(self.config,self.pdbpath)
 
         r_id=1
         for restraint in self.restraints_to_analyze:
@@ -78,19 +82,27 @@ class ReplicateToAnalyze():
         
         self.intstates=[]
         for intstatepath in self.dirs_intstates:
-            self.intstates.append(self.analyze_intstate(intstatepath))
+            self.intstates.append(self.analyze_intstate(intstatepath,args.step))
         
         subfigs=[]
         c=1
-        for intstate in self.intstates:
-            currentfig=plt.subplot(round(len(self.intstates)),2,c)
+        plt.figure(figsize=(9,9))
+        for intstate in sorted(self.intstates,key=lambda intstate:int(re.findall(r"\d+", intstate.name)[0])):
+            currentfig=plt.subplot(round(len(self.intstates)/2),2,c)
             c+=1    
-            print (intstate.results)
+            
             [currentfig.plot(intstate.results["time"],intstate.results[f"b_{i}"]) for i in range(1,1+len(self.restraints_to_analyze))]
+            currentfig.set_title(intstate.name)
+            
             subfigs.append(currentfig)
 
+        if args.csv:
+            
+            self.dump_into_csv()
+
+        plt.tight_layout()
         plt.show()
-    def analyze_intstate(self,path_to_intstate,scale=100):
+    def analyze_intstate(self,path_to_intstate,scale):
         
         self.universe=MDAnalysis.Universe(self.pdbpath,f"{path_to_intstate}/lig_in_complex.dcd")
 
@@ -112,15 +124,33 @@ class ReplicateToAnalyze():
             r_id=1
             for restraint in self.restraints_to_analyze:
                 
-                self.current_int_state.results[f"b_{r_id}"].append((self.get_distance(restraint.g1_in_cc,restraint.g2)))
+                if not args.relative_distance:
+                    self.current_int_state.results[f"b_{r_id}"].append((self.get_distance(restraint.g1_in_cc,restraint.g2)))
+                else:
+                    self.current_int_state.results[f"b_{r_id}"].append((self.get_distance(restraint.g1_in_cc,restraint.g2))-restraint.initial_distance)
             
                 r_id+=1
         
         
         return self.current_int_state
 
+    def dump_into_csv(self,csvname:str):
+        """
+        Takes the intstate results and dumps them into a csv file
+
+        Args:
+            csvname: The name of the csv to dump the data in
+        """
     def get_distance(self,g1,g2):
-        #self.logger.info(f"{g1}\n{g2}")
+        """
+        Gets the distance between the centers of mass between two atom groups
+
+        Args:
+            g1,g2: MDAnalysis AtomGroups
+        
+        Returns:
+            float: Distance between the centers of mass of the two atom groups
+        """
         tempg1=self.universe.atoms[g1.indices]
         tempg2=self.universe.atoms[g2.indices]
         p1=tempg1.center_of_mass()
